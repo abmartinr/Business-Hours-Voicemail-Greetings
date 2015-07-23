05aec6365f66f10318d97459c4e94e833a054733
Abel Martín Romero <aromero@5958-aromero-7.local>
2015-07-23 08:18:45 +0100

    Update new lines to be in html

--- business_hours.php
+++ business_hours.php
@@ -9,30 +9,30 @@ $hour = $current_datetime->format('H:i');
 $day_week = $current_datetime->format('w');
 
 $key = array_search($hour, $business_hours[$day_week]);
-echo "Script started\n";
+echo "Script started<br/>";
 
 if($key){
 	// Retrieve all numbers
 	$data = curlWrap("/channels/voice/phone_numbers.json",NULL,"GET");
 		echo ($key == "start")? "Opening the office...": "Closing the office...";
-		echo "\n";
+		echo "<br/>";
 		
 		foreach ($data->phone_numbers as $phone_number) {
 			//Put request /api/v2/channels/voice/phone_numbers/{id}.json
 			$u = "/channels/voice/phone_numbers/".$phone_number->id.".json";
 			//Replace the value
-			echo "Setting the voicemail for number ".$phone_number->number."\n";
+			echo "Setting the voicemail for number ".$phone_number->number."<br/>";
 			
 			$greetings = generateGreetingsArray($phone_number->greeting_ids, $key);
 			if($greetings){
 				$g = array("phone_number" => array("greeting_ids" => $greetings));	
 				curlWrap($u,json_encode($g),"PUT");
 			}else{
-				echo "Greeting is already set up, ignoring this update.\n";
+				echo "Greeting is already set up, ignoring this update.<br/>";
 			}
 		}
 }else{
-	echo "It's not time to change the voicemail greeting.\n";
+	echo "It's not time to change the voicemail greeting.<br/>";
 }
 
 function curlWrap($url, $json, $action){
@@ -69,7 +69,7 @@ function curlWrap($url, $json, $action){
 	curl_close($ch);
 	$decoded = json_decode($output);
 	if($action =="PUT"){
-		echo "Server response:\n" .$output. "\n";
+		echo "Server response:<br/>" .$output. "<br/>";
 	}
 	return $decoded;
 }
@@ -77,43 +77,43 @@ function curlWrap($url, $json, $action){
 function generateGreetingsArray($current_greetings,$key){
 
 	if($key=='start'){
-		echo "Setting up the start of the day voicemail\n";
+		echo "Setting up the start of the day voicemail<br/>";
 		// Let's first look if the START_GREETING is alread in the array of greetings.
 		$res = array_search(END_GREETING, $current_greetings);
 		// I'll add the START_GREETING or replace the END_GREETING
 		if($res === false){
-			echo "END_GREETING ".END_GREETING." was not found on this number.\n Current Greeting ID's:\n";
+			echo "END_GREETING ".END_GREETING." was not found on this number.<br/> Current Greeting ID's:<br/>";
 			var_dump($current_greetings);
 			// Checking that the Array doesn't contain the START_GREETING already
 			$ver = array_search(START_GREETING, $current_greetings,true);
 			if($ver === false){
-				echo "START_GREETING ".START_GREETING." was not found either on this number.\n";
-				echo "Adding START_GREETING ".START_GREETING." into array.\n";
+				echo "START_GREETING ".START_GREETING." was not found either on this number.<br/>";
+				echo "Adding START_GREETING ".START_GREETING." into array.<br/>";
 				$current_greetings[] = START_GREETING;
 			}else{
 				return false;
 			}
 		}else{
-			echo "END_GREETING found at index " . $res . "\nCurrent Greeting ID's:\n";
+			echo "END_GREETING found at index " . $res . "<br/>Current Greeting ID's:<br/>";
 			var_dump($current_greetings); 
 			// I replace the value of END_GREETING with START_GREETING
-			echo "Replacing greeting ".$current_greetings[$res]." from array with ".START_GREETING.".\n";
+			echo "Replacing greeting ".$current_greetings[$res]." from array with ".START_GREETING.".<br/>";
 			$current_greetings[$res] = START_GREETING;
 		}
 	}else{
-		echo "Setting up the end of the day voicemail\n";
+		echo "Setting up the end of the day voicemail<br/>";
 
 		// Let's look if the START_GREETING is in the array of greetings
 		$res = array_search(START_GREETING, $current_greetings);
 		// I'll add the END_GREETING or replace the START_GREETING
 		if($res === false){
-			echo "START_GREETING ".START_GREETING." was not found on this number.\n Current Greeting ID's:\n";
+			echo "START_GREETING ".START_GREETING." was not found on this number.<br/> Current Greeting ID's:<br/>";
 			var_dump($current_greetings);
 			// Checking that the Array doesn't contain the END_GREETING already
 			$ver = array_search(END_GREETING, $current_greetings);
 			if($ver === false){
-				echo "END_GREETING ".END_GREETING." was not found either on this number.\n";
-				echo "Adding END_GREETING ".END_GREETING." into array.\n";
+				echo "END_GREETING ".END_GREETING." was not found either on this number.<br/>";
+				echo "Adding END_GREETING ".END_GREETING." into array.<br/>";
 				echo "Adding greeting into array";
 				// I add the greeting to the array
 				$current_greetings[] = END_GREETING;
@@ -123,13 +123,13 @@ function generateGreetingsArray($current_greetings,$key){
 			
 		}else{
 			// I replace the value of END_GREETING with START_GREETING
-			echo "START_GREETING found at index " . $res . "\nCurrent Greeting ID's:\n";
+			echo "START_GREETING found at index " . $res . "<br/>Current Greeting ID's:<br/>";
 			var_dump($current_greetings); 
-			echo "Replacing greeting ".$current_greetings[$res]." from array with ".END_GREETING.".\n";
+			echo "Replacing greeting ".$current_greetings[$res]." from array with ".END_GREETING.".<br/>";
 			$current_greetings[$res] = END_GREETING;
 		}
 	}
-	echo "New greetings array is: \n";
+	echo "New greetings array is: <br/>";
 	var_dump($current_greetings);
 	return $current_greetings;
 }
